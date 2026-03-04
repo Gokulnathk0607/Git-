271f8deda1fae20efcff9812818b4f40da117389
GokulPC <gokulnathk0607@gmail.com>
2026-03-04 19:58:53 +0530

    Commit for git practice

---
+++ src/gitpractice/GitCommands.java
@@ -0,0 +1,15 @@
+package gitpractice;
+
+public class GitCommands {
+
+	public static void main(String[] args) {
+		 System.out.println("Common Git Commands:");
+	        System.out.println("git init");
+	        System.out.println("git add");
+	        System.out.println("git commit");
+	        System.out.println("git push");
+	        System.out.println("git pull");
+	        
+	}
+
+}
